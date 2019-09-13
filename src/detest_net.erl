87861290348efe8d5e193352dbc0354418e902aa
sergej jurecko <sergej.jurecko@wpn.tv>
2019-09-13 09:59:47 +0200

    bugfixes

--- src/detest_net.erl
+++ src/detest_net.erl
@@ -34,7 +34,7 @@ call(Msg) ->
 	end.
 
 -record(pkt,{bin = <<>>, tm = 0}).
--record(nd,{rpc_port, dist_port, offset = 0, online = true, groups = []}).
+-record(nd,{rpc_port, dist_port, offset = 0, online = true, groups = [], initialized = false}).
 -record(sock,{src_node, dst_node, other, bw = 1024 * 1024, recv_int_bytes = 0, int_budget = 1024, latency = 0, queue}).
 -record(lsock,{src_node, dst_node, accept_ref, dst_port, port, blocked = false}).
 -record(dp,{sockets = #{}, nodes = #{}, bytes = 0}).
@@ -43,7 +43,8 @@ run() ->
 	register(?MODULE,self()),
 	erlang:send_after(?INTERVAL, self(), timeout),
 	Nodes = nodes_parse(),
-	run(#dp{nodes = Nodes,
+	Nodes1 = [{NdNm, Nd#nd{initialized = true}} || {NdNm,Nd} <- maps:to_list(Nodes) ],
+	run(#dp{nodes = maps:from_list(Nodes1),
 		sockets = create_listeners(maps:to_list(Nodes),maps:to_list(Nodes),#{})}).
 run(P) ->
 	receive
@@ -55,12 +56,16 @@ run(P) ->
 			erlang:send_after(?INTERVAL, self(), timeout),
 			run(P#dp{sockets = socket_activate(maps:to_list(P#dp.sockets), P#dp.sockets)});
 		{tcp,Socket,Bin} ->
-			#sock{queue = Q} = SI = maps:get(Socket, P#dp.sockets),
-			Now = tm(),
-			NQ = queue:in(#pkt{bin = Bin, tm = Now}, Q),
-			IntBytes = SI#sock.recv_int_bytes + byte_size(Bin),
-			run(P#dp{bytes = P#dp.bytes + byte_size(Bin), 
-				sockets = (P#dp.sockets)#{Socket => SI#sock{queue = NQ, recv_int_bytes = IntBytes}}});
+			case maps:get(Socket, P#dp.sockets,undefined) of
+				undefined ->
+					run(P);
+				#sock{queue = Q} = SI ->
+					Now = tm(),
+					NQ = queue:in(#pkt{bin = Bin, tm = Now}, Q),
+					IntBytes = SI#sock.recv_int_bytes + byte_size(Bin),
+					run(P#dp{bytes = P#dp.bytes + byte_size(Bin), 
+				sockets = (P#dp.sockets)#{Socket => SI#sock{queue = NQ, recv_int_bytes = IntBytes}}})
+			end;
 		{tcp_closed,Socket} ->
 			case maps:get(Socket, P#dp.sockets,undefined) of
 				undefined ->
@@ -70,20 +75,24 @@ run(P) ->
 					run(P#dp{sockets = maps:remove(Other,maps:remove(Socket,P#dp.sockets))})
 			end;
 		{tcp_passive,Socket} ->
-			#sock{int_budget = Budget, recv_int_bytes = Bytes} = maps:get(Socket, P#dp.sockets),
-			case Budget > Bytes of
-				true ->
-					inet:setopts(Socket,[{active,once}]),
+			case maps:get(Socket, P#dp.sockets,undefined) of
+				undefined ->
 					run(P);
-				false ->
-					run(P)
+				#sock{int_budget = Budget, recv_int_bytes = Bytes} ->
+					case Budget > Bytes of
+						true ->
+							inet:setopts(Socket,[{active,once}]),
+							run(P);
+						false ->
+							run(P)
+					end
 			end;
 		{inet_async, LSock, _Ref, {ok, Sock}} ->
 			% ?INF("inet_async ~p",[LSock]),
 			run(accept(P,LSock, Sock));
-		{inet_async, LSock, _Ref, Error} ->
-			?INF("async accept error ~p",[Error]),
-			run(accept(P,LSock, undefined));
+		{inet_async, _LSock, _Ref, _Error} ->
+			?INF("async accept error ~p",[_Error]),
+			run(P);
 		Other ->
 			?INF("detest_net unknown_msg ~p",[Other]),
 			run(P)
@@ -92,6 +101,7 @@ run(P) ->
 modify(P,{group_set, Nodes, Id}) ->
 	ModNodes = [{NdNm,NI#nd{groups = butil:lists_add(Id, NI#nd.groups)}} || 
 		{NdNm,NI} <- maps:to_list(P#dp.nodes), lists:member(NdNm,Nodes) andalso lists:member(Id,NI#nd.groups) == false],
+	?INF("group_set ~p ~p ~p",[ModNodes,Nodes,P#dp.nodes ]),
 	case ModNodes of
 		[] ->
 			P;
@@ -111,13 +121,18 @@ modify(P,{group_remove, Id}) ->
 			NSocks = mod_socks(P#dp.nodes, maps:to_list(P#dp.sockets), Op, P#dp.sockets),
 			UpdatedNodes = put_all(ModNodes,P#dp.nodes),
 			P#dp{nodes = UpdatedNodes,
-				sockets = create_listeners(ModNodes,maps:to_list(UpdatedNodes),NSocks)}
+				sockets = create_listeners(maps:to_list(UpdatedNodes),maps:to_list(UpdatedNodes),NSocks)}
 	end;
 modify(P,{add_node, Node}) ->
-	#{Node := NI} = nodes_parse(),
-	Nodes = maps:put(Node, NI, P#dp.nodes),
-	P#dp{nodes = Nodes, 
-		sockets = create_listeners([{Node,NI}],maps:to_list(Nodes),P#dp.sockets)};
+	case maps:get(Node,P#dp.nodes, undefined) of
+		undefined ->
+			#{Node := NI} = nodes_parse(),
+			Nodes = maps:put(Node, NI, P#dp.nodes),
+			P#dp{nodes = Nodes, 
+				sockets = create_listeners([{Node,NI}],maps:to_list(Nodes),P#dp.sockets)};
+		_ ->
+			P
+	end;
 modify(P,{_What, Node, _Bw} = Op) ->
 	#{Node := #nd{online = NodeOnline} = NI} = P#dp.nodes,
 	case Op of
@@ -169,6 +184,7 @@ mod_socks(Nodes,[{S,SI}|T], {group_remove, GrpId} = Op, M) ->
 			end
 	end;
 mod_socks(Nodes,[{S,SI}|T], {group_set, GNodes} = Op, M) ->
+	?INF("group_set ~p",[GNodes]),
 	SrcNd = src_node(SI),
 	DstNd = dst_node(SI),
 	MemberSrc = lists:member(SrcNd, GNodes),
@@ -195,6 +211,8 @@ mod_socks(Nodes,[{S,SI}|T], {node_latency, Node, Lat} = Op, M) when SI#sock.src_
 	mod_socks(Nodes,T, Op, M#{S => SI#sock{latency = Lat}});
 mod_socks(Nodes,[{S,SI}|T], {node_online, Node, Bool} = Op, M) when SI#sock.src_node == Node; SI#sock.dst_node == Node ->
 	case Bool of
+		false when SI#sock.src_node /= SI#sock.dst_node ->
+			mod_socks(Nodes,T, Op, M);
 		false ->
 			mod_socks(Nodes,T, Op, sock_close(S, SI, M));
 		true ->
@@ -202,6 +220,8 @@ mod_socks(Nodes,[{S,SI}|T], {node_online, Node, Bool} = Op, M) when SI#sock.src_
 	end;
 mod_socks(Nodes,[{S,SI}|T], {node_online, Node, Bool} = Op, M) when SI#lsock.src_node == Node; SI#lsock.dst_node == Node ->
 	case Bool of
+		false when SI#lsock.src_node /= SI#lsock.dst_node ->
+			mod_socks(Nodes,T, Op, M);
 		false ->
 			mod_socks(Nodes,T, Op, sock_close(S,SI,M));
 		true ->
@@ -215,10 +235,11 @@ mod_socks(_,[],_, M) ->
 sock_close(S, SI, M) when element(1,SI) == sock ->
 	gen_tcp:close(S),
 	gen_tcp:close(SI#sock.other),
-	maps:delete(S,maps:delete(SI#sock.other,M));
+	maps:remove(S,maps:remove(SI#sock.other,M));
 sock_close(S, SI, M) when element(1,SI) == lsock ->
+	?INF("sock_close ~p",[SI]),
 	gen_tcp:close(S),
-	maps:delete(S,M).
+	maps:remove(S,M).
 
 flush_q(Sock, #sock{other = Other} = SI) ->
 	case queue:out(SI#sock.queue) of
@@ -237,7 +258,7 @@ flush_q(Sock, #sock{other = Other} = SI) ->
 
 socket_activate([{S,#sock{recv_int_bytes = Recv, int_budget = Budget} = SI}|T], M) ->
 	case ok of
-		_ when Recv < Budget ->
+		_ when Recv =< Budget ->
 			NRecv = 0,
 			inet:setopts(S,[{active,once}]);
 		_ ->
@@ -257,7 +278,8 @@ create_listeners([],_,M) ->
 	M.
 create_listeners(NdName,Nd,[{OtherNd,Other}|T], M) -> % when NdName /= OtherNd
 	Groups = Nd#nd.groups,
-	OtherGroups = Nd#nd.groups,
+	OtherGroups = Other#nd.groups,
+	?INF("create listeners ~p ~p ~p ~p",[NdName, OtherNd, Groups, OtherGroups]),
 	case ok of
 		_ when Groups == [], OtherGroups == [] ->
 			Doit = true;
@@ -266,10 +288,12 @@ create_listeners(NdName,Nd,[{OtherNd,Other}|T], M) -> % when NdName /= OtherNd
 			Doit = (Groups -- OtherGroups) /= Groups
 	end,
 	case Doit of
+		true when NdName == OtherNd, Nd#nd.initialized ->
+			Changes = [];
 		true ->
 			L1 = lsock(NdName, Other#nd.rpc_port + Nd#nd.offset, Other#nd.rpc_port, OtherNd),
 			L2 = lsock(NdName, Other#nd.dist_port + Nd#nd.offset, Other#nd.dist_port, OtherNd),
-			Changes = [L1,L2];
+			Changes = L1++L2;
 		false ->
 			Changes = []
 	end,
@@ -282,9 +306,14 @@ create_listeners(_,_,[],M) ->
 lsock(Nd,Port,DstPort, DstNode) ->
 	Opts = [binary, {packet, 0}, {reuseaddr, true},
             {keepalive, true}, {active, false}],
-	{ok,LSock} = gen_tcp:listen(Port, Opts),
-	{ok, Ref} = prim_inet:async_accept(LSock, -1),
-	{LSock,#lsock{accept_ref = Ref, src_node = Nd, dst_node = DstNode, dst_port = DstPort, port = Port}}.
+	case gen_tcp:listen(Port, Opts) of
+		{ok,LSock} ->
+			?INF("lsock create from=~p  to=~p, port=~p",[Nd,DstNode,Port]),
+			{ok, Ref} = prim_inet:async_accept(LSock, -1),
+			[{LSock,#lsock{accept_ref = Ref, src_node = Nd, dst_node = DstNode, dst_port = DstPort, port = Port}}];
+		_ ->
+			[]
+	end.
 
 accept(P, LSock, Sock) ->
 	case is_port(Sock) of
@@ -312,6 +341,7 @@ accept(P, LSock, Sock) ->
 		{ok,Other} ->
 			BW = ?CFG(internode_bw),
 			DF = sock_bw(#sock{queue = queue:new()}, BW),
+			?INF("Accepted sock from=~p to=~p",[LInfo#lsock.src_node, LInfo#lsock.dst_node]),
 			OI = DF#sock{other = Sock, dst_node = LInfo#lsock.src_node, src_node = LInfo#lsock.dst_node},
 			SI = DF#sock{other = Other,src_node = LInfo#lsock.src_node, dst_node = LInfo#lsock.dst_node},
 			Updates = [{LSock,LInfo#lsock{accept_ref = NewRef}},
@@ -349,7 +379,7 @@ nodes_parse() ->
 	Nodes = ?CFG(nodes),
 	% ?INF("Nodes ~p",[Nodes]),
     maps:from_list([begin
-        {butil:toatom(butil:ds_val(name,Nd)), readnd(Nd)}
+        {butil:toatom(butil:ds_val(distname,Nd)), readnd(Nd)}
     end || Nd <- Nodes]).
 
 readnd(Nd) ->
